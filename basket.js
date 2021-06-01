824bbd68bb145798fee8b4f2a89c57409f80899f
krista <krista.tomicic@gmail.com>
2021-06-01 20:20:44 +0200

    small functions implemented

--- basket.js
+++ basket.js
@@ -3,16 +3,14 @@
 import { lastStr } from "./helpers";
 import { nameId } from "./helpers";
 //everything to do with basket starts here Krista
-
 export let basket = {};
 export let orderData = [];
 document.querySelector(".amount_beers").classList.add("hide");
+
 //this function targets clicked elements to show in the list
 export function addToBasket(e) {
   const productCard = e.target.parentElement.parentElement;
   const beerName = productCard.parentNode.querySelector("h2").textContent;
-  //remove whitespaces from beerLabel to use it beerLabel as an key in obj and an html ID
-
   const beerLabel = nameId(beerName);
   const beerCount = e.target.parentElement.children[1];
   beerCount.value++;
@@ -27,9 +25,11 @@ export function addToBasket(e) {
       beerImg: productCard.parentNode.querySelector("img").src,
       beerCount: 1,
     };
+    //set key to our basket object, and add values
     basket[beerLabel] = basketItem;
   }
 
+  //beerCart interactivity
   document.querySelector(".amount_beers").classList.remove("hide");
   document.querySelector(".checkout_beer").classList.add("shake");
   document.querySelector(".amount_beers").innerHTML = "✓";
@@ -39,8 +39,7 @@ export function addToBasket(e) {
 //create HTML elements and render in basket according to data
 function showInBasket(beerLabel) {
   const item = basket[beerLabel];
-  //TODO:
-
+  //TODO: catc errored
   if (item === undefined) {
     console.log("you have 0 beers, item removed");
   }
@@ -49,6 +48,7 @@ function showInBasket(beerLabel) {
   const quantity = Number(item.beerCount);
   const cardCopy = document.createElement("div");
   cardCopy.setAttribute("class", "cardItem");
+  //set ID to html elements same as obj key- it is for basket manipulation
   cardCopy.setAttribute("id", beerLabel);
   cardCopy.innerHTML = `<div class ="beer-card">
   <img src="${item.beerImg}"  class= "basketImg" alt="" />
@@ -67,9 +67,8 @@ function showInBasket(beerLabel) {
         </div>
        `;
 
-  //give same ID to html element as Key in basket obj
   const cardBeerName = document.querySelector("#" + nameId(beerLabel));
-  //if same beer clicked >1 add up only price and quantyty, not whole new beer order card
+  //if same beer clicked >1 add up only price and quantyty to basket, not whole new beer order card
   if (cardBeerName != null) {
     cardBeerName.remove();
   }
@@ -87,7 +86,7 @@ function showInBasket(beerLabel) {
   showTotalPrice();
 }
 
-//edit items already in basket on +
+//edit items already in basket on +btn
 function editBasketPlus(e) {
   const beerLabel = e.target.parentElement.parentElement.id;
   const beerCount = e.target.parentElement.children[1];
@@ -107,7 +106,6 @@ function editBasketPlus(e) {
 function editBasketMinus(e) {
   const beerLabel = e.target.parentElement.parentElement.id;
   const beerCount = e.target.parentElement.children[1];
-  console.log(beerCount);
   const calcPrice = e.target.parentElement.parentElement.querySelector("h6");
   beerCount.value--;
   if (beerLabel in basket && basket[beerLabel].beerCount > 1) {
@@ -132,14 +130,11 @@ function showTotalPrice() {
     //turn string into number and push to array total
     total.push(Number(price));
   });
-
-  //reduce mUST take two params- accumulator what we are returning and current what we are looping through, and also startin point a number
+  //reduce takes two params- accumulator what we are returning and current what we are looping through, and also starting point which is here 0
   const totalPrice = total.reduce((total, beerItemNr) => {
     total += beerItemNr;
-
     return total;
   }, 0);
-  // console.log(totalPrice);
   document.querySelector("#total").innerHTML = `DKK ${totalPrice}`;
 }
 
@@ -150,17 +145,14 @@ export function removeFromBasket(e) {
   console.log(beerLabel);
   const beerCount = e.currentTarget.parentElement.children[1];
   beerCount.value--;
-  console.log(beerCount.value);
-  // //beerkeg number
-  document.querySelector(".amount_beers").innerHTML = "-";
-  //value in minus box is not going under 0
+  document.querySelector(".amount_beers").innerHTML = "✓ ";
+  //prevent value in minus box to go under 0
   if (beerCount.value < 1) {
     beerCount.value = 0;
     beerCount.disabled = true;
     document.querySelector(".checkout_beer").classList.remove("shake");
     document.querySelector(".amount_beers").classList.add("hide");
   }
-
   //subtract 1 from what is current number of beers
   if (beerLabel in basket) {
     basket[beerLabel].beerCount -= 1;
@@ -170,24 +162,24 @@ export function removeFromBasket(e) {
     const beerInBasket = document.querySelector("#" + nameId(beerLabel));
     console.log(beerInBasket);
     beerInBasket.remove();
+    //thorws undefined in console because it is removed from object, and we are calling showInBasket()
     delete basket[beerLabel];
     showTotalPrice();
   }
   showInBasket(beerLabel);
-  console.log(beerLabel);
 }
 
+//post data to server
 export function postOrder(e) {
+  // e is on form
   e.preventDefault();
   orderData = [];
-  console.log(basket);
   const keys = Object.keys(basket);
   keys.forEach((key) => {
     orderData.push({
       name: basket[key].beerName,
       amount: basket[key].beerCount,
     });
-    console.log(orderData);
   });
 
   const postData = JSON.stringify(orderData);
@@ -200,5 +192,5 @@ export function postOrder(e) {
     body: postData,
   })
     .then((res) => res.json())
-    .then((orderData) => console.log(orderData));
+    .then((postData) => console.log(postData));
 }
